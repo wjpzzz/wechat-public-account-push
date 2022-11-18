7e8d87ab64b53db46e1bbeacab43b5ddf2843aa3
wjpzzz <78393900+wjpzzz@users.noreply.github.com>
2022-11-18 21:27:02 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -34,15 +34,18 @@ const USER_CONFIG = {
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         
         {
+          type: '节日', name: 'coming day!', year: '2022', date: '12-24',
+        },
+        {
           type: '节日', name: '相识纪念日', year: '2019', date: '03-04',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
+        // 认识的日子
+        { keyword: 'love_day', date: '2019-03-04' },
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2022-01-02' },
       ],
     },
   ],
