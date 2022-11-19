479e4d48e4afb0e213a92b538f68f64b76603794
wjpzzz <78393900+wjpzzz@users.noreply.github.com>
2022-11-19 21:39:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -15,35 +15,79 @@ const USER_CONFIG = {
 
   PROVINCE: '山东',
   CITY: '济南',
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
 
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: false,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: false,
+    
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: false,
+    
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: false,
+    
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+    
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+    
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+  
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+  
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
   USERS: [
     {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oM8Ij5-NmK2kq2VxeRAQUZRshf-k',
+      id: 'oM8Ij50e0222GzB3qysS6Hsw3tW0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'SFPQdtJMNAXJX62Svf1uXZ7mcPRXx-ms2plYbi3ni2E',
+      useTemplateId: '1W1j6YQl-KSANjmjhSdp1D1HGHKoGaU5sYKsdmyE8B0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-01',
+      horoscopeDateType: '今日',
+      openUrl: 'https://weibo.com/hot/search',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1998', date: '04-06',
+          type: '*生日', name: 'beaubeau宝贝', year: '1998', date: '04-06',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         
         {
-          type: '节日', name: 'coming day!', year: '2022', date: '12-24',
+          type: '节日', name: 'cccoming day!', year: '2022', date: '12-24',
         },
         {
           type: '节日', name: '相识纪念日', year: '2019', date: '03-04',
+        },
+        {
+          type: '节日', name: '家人们在一起纪念日', year: '2019', date: '03-04',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 认识的日子
-        { keyword: 'love_day', date: '2022-01-02' },
+        { keyword: 'love_day', date: '2019-03-04' },
         // 在一起的日子
         { keyword: 'marry_day', date: '2022-01-02' },
       ],
@@ -52,14 +96,15 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '1W1j6YQl-KSANjmjhSdp1D1HGHKoGaU5sYKsdmyE8B0',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
-    }
+      id: 'oM8Ij5-NmK2kq2VxeRAQUZRshf-k',
+    },
+    
   ],
 
 }
